e00f6e1b69e96d2e4aeba25e505281980c4f1093
la-gatsby <50734208+la-gatsby@users.noreply.github.com>
2024-05-07 21:12:09 +0530

    Implement neural network

--- car.js
+++ car.js
@@ -1,3 +1,4 @@
+
 class Car{
     constructor(x,y,width,height,controlType,maxSpeed=3){
         this.x=x;
@@ -12,21 +13,37 @@ class Car{
         this.angle=0;
         this.damaged=false;
 
+        this.useBrain=controlType=="AI";
+
         if(controlType!="DUMMY"){
             this.sensor=new Sensor(this);
+            this.brain=new NeuralNetwork(
+                [this.sensor.rayCount,6,4]
+            );
         }
         this.controls=new Controls(controlType);
     }
 
     update(roadBorders,traffic){
         if(!this.damaged){
-        this.#move();
-        this.polygon=this.#createPolygon();
-        this.damaged=this.#assessDamage(roadBorders,traffic);
-    }  
-    if(this.sensor){      
-        this.sensor.update(roadBorders,traffic);
-    }
+            this.#move();
+            this.polygon=this.#createPolygon();
+            this.damaged=this.#assessDamage(roadBorders,traffic);
+        }
+        if(this.sensor){
+            this.sensor.update(roadBorders,traffic);
+            const offsets=this.sensor.readings.map(
+                s=>s==null?0:1-s.offset
+            );
+            const outputs=NeuralNetwork.feedForward(offsets,this.brain);
+
+            if(this.useBrain){
+                this.controls.forward=outputs[0];
+                this.controls.left=outputs[1];
+                this.controls.right=outputs[2];
+                this.controls.reverse=outputs[3];
+            }
+        }
     }
 
     #assessDamage(roadBorders,traffic){
@@ -51,17 +68,14 @@ class Car{
             x:this.x-Math.sin(this.angle-alpha)*rad,
             y:this.y-Math.cos(this.angle-alpha)*rad
         });
-
         points.push({
             x:this.x-Math.sin(this.angle+alpha)*rad,
             y:this.y-Math.cos(this.angle+alpha)*rad
         });
-
         points.push({
             x:this.x-Math.sin(Math.PI+this.angle-alpha)*rad,
             y:this.y-Math.cos(Math.PI+this.angle-alpha)*rad
         });
-
         points.push({
             x:this.x-Math.sin(Math.PI+this.angle+alpha)*rad,
             y:this.y-Math.cos(Math.PI+this.angle+alpha)*rad
@@ -76,12 +90,14 @@ class Car{
         if(this.controls.reverse){
             this.speed-=this.acceleration;
         }
+
         if(this.speed>this.maxSpeed){
             this.speed=this.maxSpeed;
         }
         if(this.speed<-this.maxSpeed/2){
             this.speed=-this.maxSpeed/2;
         }
+
         if(this.speed>0){
             this.speed-=this.friction;
         }
@@ -91,10 +107,10 @@ class Car{
         if(Math.abs(this.speed)<this.friction){
             this.speed=0;
         }
-       
+
         if(this.speed!=0){
-        const flip=this.speed>0?1:-1
-               if(this.controls.left){
+            const flip=this.speed>0?1:-1;
+            if(this.controls.left){
                 this.angle+=0.03*flip;
             }
             if(this.controls.right){
@@ -105,7 +121,6 @@ class Car{
         this.x-=Math.sin(this.angle)*this.speed;
         this.y-=Math.cos(this.angle)*this.speed;
     }
-    //draw car method
 
     draw(ctx,color){
         if(this.damaged){
